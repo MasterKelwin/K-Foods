12f1107af5115e1272d115e31d5264923f5effd0
MasterKelwin <mkelwin@outlook.com>
2022-08-31 06:19:03 -0300

    some css on button to be more pretty

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.button `
+    font-family: 'Source Sans Pro', sans-serif;
     width: auto;
     height: 1.8rem;
     border-radius: 8px;
     background-color: #ebebeb;
     font-weight: bold;
     border: none;
+    padding: 0 1rem;
+    text-transform: uppercase;
     &:hover{
         cursor: pointer;
     }
