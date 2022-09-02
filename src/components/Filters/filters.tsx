7978466998c218bc08b7f6b22e83ec0c77136667
MasterKelwin <mkelwin@outlook.com>
2022-09-02 17:52:48 -0300

    buttons added

--- src/components/Filters/filters.tsx
+++ src/components/Filters/filters.tsx
@@ -1,14 +1,15 @@
 import { Wrapper } from './style';
 import filter from './filters.json';
+import Button from '../Button/button';
 
 type IOption = typeof filter[0];
 
 export default function Filters() {
-    function banana(option: IOption) {}
+    function selectOption(option: IOption) {}
     return (
         <Wrapper>
             {filter.map((option) => (
-                <label>{option.label}</label>
+                <Button>{option.label}</Button>
             ))}
         </Wrapper>
     )
--- src/components/Filters/style.ts
+++ src/components/Filters/style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.ul `
-
+    padding: 0;
 `
