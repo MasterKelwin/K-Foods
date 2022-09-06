a2c23c6a39c2a370b07f9e4b69a16402c3c9c731
MasterKelwin <mkelwin@outlook.com>
2022-09-06 09:50:59 -0300

    cards checkpoint

---
+++ src/components/Menu/Cards/Cards.tsx
@@ -0,0 +1,9 @@
+import { Wrapper } from './style'
+
+export default function Cards() {
+    return (
+        <Wrapper>
+            <h1>alou</h1>
+        </Wrapper>
+    )
+}
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Wrapper } from './style';
 import Finder from './Finder/Finder';
 import Filters from 'components/Menu/Filters/filters';
+import Cards from './Cards/Cards';
 
 export default function Menu() {
   const [search, setSearch] = useState<string>("");
@@ -12,6 +13,9 @@ export default function Menu() {
           <h1>Cardápio</h1>
           <Finder search={search} setSearch={setSearch} />
           <Filters filter={filter} setFilter={setFilter}/>
+        </div>
+        <div>
+          <Cards />
         </div>
       </Wrapper>
   )
