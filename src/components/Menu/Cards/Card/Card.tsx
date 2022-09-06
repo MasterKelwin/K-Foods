81866b83900e073d18dc908ac215e68636489a78
MasterKelwin <mkelwin@outlook.com>
2022-09-06 15:32:47 -0300

    single card done

--- src/components/Menu/Cards/Card/Card.tsx
+++ src/components/Menu/Cards/Card/Card.tsx
@@ -4,7 +4,18 @@ export default function Card() {
     return (
         <Wrapper>
             <DivImgPizza></DivImgPizza>
-            <DivInfo></DivInfo>
+            <DivInfo>
+                <div>
+                    <h1>Super Vegetariana</h1>
+                    <p>Massa fina, molho de tomate, pimentão, tomate seco, cebola, cogumelo champgnon, pimentão verde, agrião e queijo.</p>
+                </div>
+                <ul>
+                    <li>Vegetarianas</li>
+                    <li>500g</li>
+                    <li>Serve 2 pessoas</li>
+                    <li>R$ 50,00</li>
+                </ul>
+            </DivInfo>
         </Wrapper>
     )
 }
--- src/components/Menu/Cards/Card/style.ts
+++ src/components/Menu/Cards/Card/style.ts
@@ -1,12 +1,23 @@
 import styled from 'styled-components';
+import Vegetariana from 'assets/flavors/vegetariana.jpg'
 
 export const Wrapper = styled.div`
     border-radius: 16px;
-    width: 100%;
+    width: 80%;
     height: 160px;
     background-color: #d3d1d1;
     display: flex;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;
+    margin: 0 0 1rem 0;
+    transition: .5s ease-in-out;
+    position: absolute;
+    left: 10%;
+    &:hover {
+        width: 82%;
+        position: absolute;
+        left: 9%;
+        
+    }
     
 `
 
@@ -14,10 +25,41 @@ export const Wrapper = styled.div`
     border-radius: 16px;
     width: 160px;
     height: 160px;
-    background-color: blue;
+    background-image: url(${Vegetariana});
+    background-size: cover;
 `
 
 
     export const DivInfo = styled.div`
     width: auto;
+    margin: 1rem;
+
+    h1 {
+        font-size: 2rem;
+        font-weight: bold;
+        margin-bottom: 8px;
+    }
+
+    ul {
+        display: flex;
+        margin-top: 1.5rem;
+        margin-left: .5rem;
+
+        li {
+            margin-right: 1rem;
+            background-color: none;
+            font-weight: bold;
+            padding: .5rem;
+            border-radius: 8px;
+            border: solid 2px;
+            transition: .5s ease-in-out;
+            &:hover{
+                background-color: #490707;
+                border: solid 2px #ebebeb;
+                color: #ebebeb;
+                cursor: pointer;
+                text-transform: uppercase;
+            }
+        }
+    }
 `
