bfe4b69f395b1f42168fe626ef19534a1f7d3886
MasterKelwin <mkelwin@outlook.com>
2022-09-13 10:34:06 -0300

    final build

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -6,8 +6,8 @@ export default function footer() {
             <h1>Desenvolvido por Kelwin Hollunder</h1>
             <div>
                 <a href="https://github.com/MasterKelwin" rel='noreferrer' target='_blank'> GITHUB </a>
-                <a href="https://github.com/MasterKelwin" rel='noreferrer' target='_blank'> INSTAGRAM </a>
-                <a href="https://github.com/MasterKelwin" rel='noreferrer' target='_blank'> LINKEDIN </a>
+                <a href="https://www.instagram.com/homem_javascript/?hl=pt-br" rel='noreferrer' target='_blank'> INSTAGRAM </a>
+                <a href="https://www.linkedin.com/in/kelwin-hollunder/" rel='noreferrer' target='_blank'> LINKEDIN </a>
             </div>
         </Wrapper>
     )
